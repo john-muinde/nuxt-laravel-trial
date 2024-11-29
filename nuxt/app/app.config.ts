b5b52f66ff2661a0a705f41019f04fda6d1ec214
John Muinde <66753112+john-muinde@users.noreply.github.com>
2024-11-29 06:08:31 +0000

    Initial commit

---
+++ nuxt/app/app.config.ts
@@ -0,0 +1,15 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'emerald',
+    strategy: 'override',
+    gray: 'neutral',
+    container: {
+      constrained: 'max-w-7xl w-full'
+    },
+    avatar: {
+      default: {
+        icon: 'i-heroicons-user',
+      }
+    }
+  }
+})
